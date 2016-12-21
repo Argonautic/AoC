f28e71560296971df349383907041dac38940b05
Argonautic <jonathan.zhou93@gmail.com>
2016-12-21 09:53:36 -0500

    Add files via upload

---
+++ AoC 6.2.py
@@ -0,0 +1,22 @@
+f = open('AoC6_input.txt')
+puzzleinput = f.read()
+f.close
+
+import collections
+
+def repair(inpt):
+    result = ''
+    index = 0
+    rows = inpt.split()
+    column_message = ''
+    while index < len(rows[0]):
+        for row in rows:
+            column_message += row[index]
+        frequencies = collections.Counter(column_message)
+        highest_frequency = min(frequencies, key=frequencies.get)
+        result += highest_frequency
+        index += 1
+        column_message = ''
+    return result
+
+print(repair(puzzleinput))
