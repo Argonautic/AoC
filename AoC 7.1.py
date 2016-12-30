3b122a5596abf9dae5b1cd5b8f7c33a609030869
Argonautic <jonathan.zhou93@gmail.com>
2016-12-30 12:56:05 -0500

    Update AoC 7.1.py

--- AoC 7.1.py
+++ AoC 7.1.py
@@ -4,39 +4,26 @@ f = open('C:/Users/jzhou/Desktop/AoC/AoC7_input.txt')
 puzzleinput = f.read()
 f.close()
 
-testinput = """rhamaeovmbheijj[hkwbkqzlcscwjkyjul]ajsxfuemamuqcjccbcpoop
-gdlrknrmexvaypu[crqappbbcaplkkzb]vhvkjyadjsryysvj[nbvypeadikilcwg]jwxlimrgakadpxu[dgoanojvdvwfabtt]yqsalmulblolkgsheo
-dqpthtgufgzjojuvzvm[eejdhpcqyiydwod]iingwezvcbtowwzc[uzlxaqenhgsebqskn]wcucfmnlarrvdceuxqc[dkwcsxeitcobaylhbvc]klxammurpqgmpsxsr
-gmmfbtpprishiujnpdi[wedykxqyntvrkfdzom]uidgvubnregvorgnhm"""
-
-test = 'rhamaeovmbheijj[hkwbkqzlcscwjkyjul]ajsxfuemamuqcjccbcpoop'
-
-def contains_abba(string):
-    abba_start = re.search('(\w)(\w)\\2\\1',string)
-    print(abba_start)
-    try:
-        abba = string[abba_start:abba_start+4]
-        print(abba)
+def contains_abba(string): #to verify that a string contains an abba pattern
+    abba = re.search('(\w)(\w)\\2\\1',string)
+    if abba != None:
+        abba = abba.group(0)
         if abba[0] != abba[1]:
             return True
-    except:
-        return False
+    return False
 
 def TLScount(inpt):
     result = 0
     IPs = inpt.split()
     for IP in IPs:
-        valid = True
-        hypernet_strings = re.findall('(?<=\[)\w+(?=\])',IP)
+        valid = True #valid gauges whether or not a hypernet string in the input contains an abba or not
+        hypernet_strings = re.findall('(?<=\[)\w+(?=\])',IP) #negative lookahead and lookbehind regex statement
         for string in hypernet_strings:
             if contains_abba(string):
                 valid = False
-                break
+                break #all it takes it one hypernet abba to spoil the barrel
         if contains_abba(IP) and valid == True:
             result += 1
-            print(hypernet_strings)
-            print(IP)
     return result
 
-print(contains_abba(test))
-#print(TLScount(testinput))
+print(TLScount(puzzleinput))
