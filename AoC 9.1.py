b72e14b3d81ec81c57d2daf6bb022e48b4c0c00f
Argonautic <jonathan.zhou93@gmail.com>
2017-02-16 14:58:50 -0500

    Update AoC 9.1.py

--- AoC 9.1.py
+++ AoC 9.1.py
@@ -1,25 +1,18 @@
 import regex as re
 
-f = open('C:/Users/jzhou/Desktop/AoC/Puzzleinputs/AoC9_input.txt')
+f = open("C:/Users/Jonathan/Documents/Programming/Advent of Code/AoC9_input.txt")
 puzzleinput = f.read()
 f.close()
 
-print(len(puzzleinput))
-
-h = 'la(5x3)(5x5)Tomfsd(2x1)Hoshtrhgfdbhs(10x10)dgfsdgfsdfsrrtfhj'
-
-def determine_length(inpt):
+def determine_length(inpt): #determines the length of the decompressed file
     result = ''
     while re.search('\(\d+x\d+\)', inpt):
         marker = re.search('\(\d+x\d+\)', inpt) #the matchobject that returns the first pattern match
         markerText = marker[0] #the string of the matchobject
         depth, repetitions = int(markerText[1:markerText.find('x')]), int(markerText[markerText.find('x') + 1:markerText.find(')')]) #to find the depth and # of repetitions of the next repetition
-        toDuplicate = inpt[marker.end():marker.end() + depth]
+        toDuplicate = inpt[marker.end():marker.end() + depth] #the string to duplicate
         duplication = toDuplicate * repetitions
-        result, inpt = result + inpt[:marker.start()] + duplication, inpt[marker.end() + depth:]
-        #print(result)
-        #print(inpt)
-    result += inpt
+        result, inpt = result + inpt[:marker.start()] + duplication, inpt[marker.end() + depth:] #to prepare the input for the next iteration
+    if not ' ' in inpt: #to add in any leftover characters not caught by a compressor, and that isn't whitespace
+        result += inpt
     return len(result)
-
-print(determine_length(h))
